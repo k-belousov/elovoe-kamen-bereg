7cc9b10370a07f281950ba3b27cfb3654f72832a
k-belousov <k.belousov.2gis@gmail.com>
2025-05-27 03:16:33 +0500

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,12 +3,14 @@
 import React from 'react';
 
 const Map = () => {
-  const yandexMapUrl = 'https://yandex.ru/maps/-/CHCWrHoc';
-  const googleMapsUrl = 'https://www.google.com/maps/place/Каменный+Берег/@55.0053557,60.315461,17z/data=!3m1!4b1!4m6!3m5!1s0x43c56bef9e487133:0x18901042074edcb8!8m2!3d55.0053557!4d60.3180498!16s%2Fg%2F11s7ts5j_1?entry=ttu';
-  const twoGisUrl = 'https://2gis.ru/chebarkul/firm/70030076176875106';
+  const yandexMapUrl =
+    'https://yandex.ru/maps/?ll=60.855114%2C55.041500&mode=routes&rtext=55.159902%2C61.402554~55.004665%2C60.316781&rtt=auto&ruri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1MzE1OTUyNxIg0KDQvtGB0YHQuNGPLCDQp9C10LvRj9Cx0LjQvdGB0LoiCg04nHVCFb2jXEI%2C~ymapsbm1%3A%2F%2Forg%3Foid%3D203268538693&z=10.3';
 
-  const iframeSrc =
-    'https://yandex.ru/map-widget/v1/?ll=60.855114%2C55.041500&mode=routes&rtext=55.1644419%2C61.4368432~ymapsbm1%3A%2F%2Forg%3Foid%3D203268538693&rtt=auto&z=10.3';
+  const googleMapsEmbedUrl =
+    'https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d177327.384051753!2d60.54801557413216!3d55.07546969945217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x43c592cb104a3a8d%3A0xef224a2a6d1711bf!2z0KfQtdC70Y_QsdC40L3RgdC6LCDQp9C10LvRj9Cx0LjQvdGB0LosINCg0L7RgdGB0LjRjw!3m2!1d55.1644419!2d61.4368432!4m5!1s0x43c56bef9e487133%3A0x18901042074edcb8!2z0JrQsNC80LXQvdGB0LjQstC90LAg0JHQtNC10YDQvtC7LCDQp9C10LvRj9Cx0LjQvdGB0LrQsNGPINC-0LHQu9Cw0YHRgtGMLCA0NTY0NDE!3m2!1d55.0053557!2d60.3180498!5e0!3m2!1sru!2sru!4v1718897822216!5m2!1sru!2sru';
+
+  const twoGisUrl =
+    'https://2gis.ru/chelyabinsk/directions/points/61.402709%2C55.157389%3B2111685095587875%7C60.315461%2C55.005338%3B70030076176875106?m=60.859669%2C55.040166%2F10.45&immersive=on';
 
   return (
     <section id="map" className="py-12 bg-nature-green-50">
@@ -37,10 +39,10 @@ const Map = () => {
               Открыть в Яндекс.Картах
             </a>
             <a
-              href={googleMapsUrl}
+              href="https://www.google.com/maps/dir//55.0053557,60.3180498"
               target="_blank"
               rel="noopener noreferrer"
-              className="px-4 py-2 rounded-lg bg-gray-600 text-white text-sm font-medium hover:bg-gray-700"
+              className="px-4 py-2 rounded-lg bg-blue-600 text-white text-sm font-medium hover:bg-blue-700"
             >
               Открыть в Google Maps
             </a>
@@ -57,14 +59,15 @@ const Map = () => {
           {/* Встраиваемая карта */}
           <div className="h-96 mt-4">
             <iframe
-              src={iframeSrc}
+              src={googleMapsEmbedUrl}
               width="100%"
               height="100%"
-              frameBorder="0"
+              style={{ border: 0 }}
               allowFullScreen
               loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
               className="w-full h-full"
-              title="Карта проезда к базе отдыха Каменный берег"
+              title="Google Map"
             ></iframe>
           </div>
         </div>
